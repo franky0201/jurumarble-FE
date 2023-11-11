372937de7b3b9f737c181d2d0a702c0877258727
franky0201 <franky0201@naver.com>
2023-11-12 01:55:54 +0900

    feat:게임판 수정 1차
    그러하다

--- select/asset/script.js
+++ select/asset/script.js
@@ -11,22 +11,11 @@ function howmany(n) {
   const container = document.querySelector(".container");
   let str = "";
   for (let i = 1; i < n + 1; i++) {
-    // const div = document.createElement("div");
 
-    // const img = document.createElement("img");
-    // img.src = `./static/cap_${i}.png`;
-
-    // const input = document.createElement("input");
-    // input.type = "text";
-
-    // div.appendChild(img);
-    // div.appendChild(input);
-
-    // container.appendChild(div);
 
     str += `
         <div class="user">
-            <img src="./static/cap_${i}.png" />
+            <img src="./static/cap${i}.png" />
             <input type="text" placeholder="플레이어명(팀명)"/>
         </div>
     `;
