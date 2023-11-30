303a5eb261da5b7db2a5021403c68345fff36745
franky0201 <franky0201@naver.com>
2023-12-01 04:53:27 +0900

    feat:바퀴수 추가
    그러함

--- select/asset/script.js
+++ select/asset/script.js
@@ -7,30 +7,47 @@ window.onload = function () {
   }
 };
 
+let count = 1; // 초기 값은 1로 설정합니다.
+
 function howmany(n) {
   const container = document.querySelector(".container");
   let str = "";
   for (let i = 1; i < n + 1; i++) {
-    // const div = document.createElement("div");
-
-    // const img = document.createElement("img");
-    // img.src = `./static/cap_${i}.png`;
-
-    // const input = document.createElement("input");
-    // input.type = "text";
-
-    // div.appendChild(img);
-    // div.appendChild(input);
-
-    // container.appendChild(div);
-
     str += `
-        <div class="user">
-            <img src="./static/cap${i}.png" />
-            <input type="text" placeholder="플레이어명(팀명)"/>
-        </div>
+      <div class="user">
+        <img src="./static/cap${i}.png" />
+        <input type="text" placeholder="플레이어명(팀명)"/>
+      </div>
     `;
   }
-  container.innerHTML =
-    str + `<button id="btn" onclick="seletcion()">설정</button>`;
+  // +와 - 버튼 및 '몇바퀴?' 문구를 가로로 나열
+  str += `
+    <div id="play-count">
+      <span>몇바퀴 돌래?</span>
+      <button id="minus" class="btn">-</button>
+      <span id="count">${count}</span>
+      <button id="plus" class="btn">+</button>
+    </div>
+    <button id="btn" onclick="seletcion()">설정</button>
+  `;
+
+  container.innerHTML = str;
+
+  // +와 - 버튼에 클릭 이벤트 추가
+  document.getElementById("plus").addEventListener("click", increment);
+  document.getElementById("minus").addEventListener("click", decrement);
+}
+
+function increment() {
+  if (count < 3) count++;
+  updateDisplay();
+}
+
+function decrement() {
+  if (count > 1) count--;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("count").innerText = count;
 }
